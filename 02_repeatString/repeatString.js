3bfc11769220c988972556bc066c841bfdb1e748
Matthew Thomas <adriant172@gmail.com>
2022-04-27 23:40:32 +0900

    Complete 02_repeatString excercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let new_string = string;
+    if (num === 0) {
+        return "";
+    } else if (num < 0){
+        return "ERROR"
+    }
+    for (let i = 0; i < num - 1; i++){
+        new_string += string;
+    }
+    return new_string;
 };
 
 // Do not edit below this line
