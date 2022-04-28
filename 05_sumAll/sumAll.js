4ec57795ef465eb487021717d59735b61843c35e
Matthew Thomas <adriant172@gmail.com>
2022-04-29 01:24:25 +0900

    Complete 05_sumAll excercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
 const sumAll = function() {
-
+    let beginNum;
+    let endNum;
+    if (arguments[0] < 0 || arguments[1] < 0){
+        return "ERROR";
+    }
+    if (typeof arguments[0] !== "number" || typeof arguments[1] !== "number"){
+        return "ERROR";
+    }
+    if (arguments[0] < arguments[1]) {
+        beginNum = arguments[0];
+        endNum = arguments[1];
+    } else {
+        beginNum = arguments[1];
+        endNum = arguments[0];
+    }
+    let totalSum = endNum;
+    for (let i = beginNum; i < endNum; i++) {
+        totalSum += i;
+    }
+    return totalSum 
 };
 
 // Do not edit below this line
