2d204235d5d3f372c9fc241db54e833796397564
Matthew Thomas <adriant172@gmail.com>
2022-04-29 00:51:42 +0900

    Complete 04_removeFromArray excercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    let new_array = []
+    for (let i = 1; i < arguments.length; i++) {
+        array = array.filter(item => item !== arguments[i]);
+        
+    }
+    return array;
 };
 
 // Do not edit below this line
