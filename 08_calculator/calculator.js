a68e6ef376881df1fd5df373f0af224a82563378
Matthew Thomas <adriant172@gmail.com>
2022-05-17 00:00:11 +0900

    Complete 08_calculator excercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
 const add = function() {
-	
+  const result = arguments[0] + arguments[1];
+  return result;
 };
 
 const subtract = function() {
+	const result = arguments[0] - arguments[1];
+  return result;
+};
+
+const sum = function(array) {
+  let total = 0;
+  for (number of array) {
+    total += number;
+  }
+  return total;
 	
 };
 
-const sum = function() {
+const multiply = function(array) {
+  let total = 1;
+  if (!array) {
+    return
+  }
+  for (number of array) {
+    total = total * number;
+  }
+  return total;
+
+};
+
+const power = function(num, power) {
+  let result = 1;
+  for (let i = 0; i < power; i++) {
+    result = result * num;
+  }
+  return result;
 	
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num == 0 || num == 1) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
 };
 
 // Do not edit below this line
