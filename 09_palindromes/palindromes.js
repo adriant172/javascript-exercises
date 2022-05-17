3560704b0fa317ee368f536ad7b0f0f8f1f13d56
Matthew Thomas <adriant172@gmail.com>
2022-05-18 00:27:39 +0900

    Complete 09_palindromes excercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let newString = [];
+    for (char of string) {
+        if ((/[a-zA-Z]/).test(char) === true) {
+            newString += char.toLowerCase();
+        } else {
+            continue;
+        }
+    }
+    const stopPoint = Math.ceil(newString.length / 2)
+    for (let i = 1; i < stopPoint; i++) {
+        if (newString[i - 1] !== newString[newString.length - i]) {
+            return false;
+        } 
+        
+    }
+    return true;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+//Palindrome length has to be an odd number since center character has not duplicate
