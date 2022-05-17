48bf2aff7897ddb5e1cf00877351027b82626f6d
Matthew Thomas <adriant172@gmail.com>
2022-05-18 00:57:20 +0900

    Complete 10_fibonacci excercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(fibNum) {
+    fibNum = parseInt(fibNum);
+    let numList = [1, 1];
+    if (fibNum < 1) {
+        return "OOPS"
+    }
+    for (let i = 0; i < fibNum; i++) {
+        const addends = numList.slice(i, i + 2);
+        newNum = addends[0] + addends[1];
+        numList.push(newNum);
+    }
+    return numList[fibNum - 1];
 
 };
 
