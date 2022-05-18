97bf4088cc20e1ad4e5b1390615b153fbe0aed83
Matthew Thomas <adriant172@gmail.com>
2022-05-18 23:03:05 +0900

    Complete 12_findTheOldest excercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldest = people.reduce((oldestPerson, person) => {
+        if ("yearOfDeath" in person === false ){
+            person.yearOfDeath = new Date().getFullYear();
+        }
+        const age = person.yearOfDeath - person.yearOfBirth;
+        if (age > oldestPerson.yearOfDeath - oldestPerson.yearOfBirth) {
+            return person;
+        } else {
+            return oldestPerson;
+        }
+    }, {
+        name: "name",
+        yearOfBirth: 0,
+        yearOfDeath: 0,
+
+    })
+    return oldest;
 
 };
 
